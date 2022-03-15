ca3bc83ecc95bd683d189e482dc8ac1d4b2ff3fc
Markus Trachsel <markus@codechefs.ch>
2022-03-15 16:19:38 +0100

    Fixed a warning and added Stats

--- Service/Data/DataViewModel.cs
+++ Service/Data/DataViewModel.cs
@@ -1,5 +1,12 @@
 namespace github_branch_lifetime.Data;
 public class BranchViewModel
 {
+    public BranchViewModel()
+    {
+        Branches = new List<Branch>();
+    }
+
     public List<Branch> Branches { get; set; }
+    public decimal AverageBranchLifespanInDaysTotal { get; set; }
+    public decimal AverageBranchLifespanInDaysLast3Months { get; set; }    
 }
